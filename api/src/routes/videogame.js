85f7f86970733c332518a8dff4d1b3e665267fca
Andyvaz1 <andyvaz192@gmail.com>
2022-07-22 15:35:41 -0300

    routes casi terminada

--- api/src/routes/videogame.js
+++ api/src/routes/videogame.js
@@ -11,43 +11,43 @@ let url = `https://api.rawg.io/api/games`;
 
 router.get("/", async (req, res) => {
     if (!req.query) {
-        const response = await axios.get(url);
-        const apiVideogame = response.data.results;
-        const resVideogame = [];
-        apiVideogame.map((game) => {
-            let obj = {
-                id: game.id,
-                name: game.name,
-                background_image: game.background_image,
-                genres: game.genres,
-            };
-            resVideogame.push(obj);
-        });
-
         try {
+            const response = await axios.get(url);
+            const apiVideogame = response.data.results;
+            const resVideogame = [];
+            apiVideogame.map((game) => {
+                let obj = {
+                    id: game.id,
+                    name: game.name,
+                    background_image: game.background_image,
+                    genres: game.genres,
+                };
+                resVideogame.push(obj);
+            });
+
             res.json(resVideogame);
         } catch (error) {
             res.send(error);
         }
     } else {
         const { name } = req.query;
-        const response = await axios.get(
-            url.concat(`?key=${API_KEY}&search=${name}`)
-        );
-        const apiVideogame = response.data.results;
-        const resVideogame = [];
-        apiVideogame.map((game) => {
-            let obj = {
-                id: game.id,
-                name: game.name,
-                background_image: game.background_image,
-                genres: game.genres,
-            };
-            resVideogame.push(obj);
-        });
-        const quinceResult = resVideogame.slice(0, 15);
-
         try {
+            const response = await axios.get(
+                url.concat(`?key=${API_KEY}&search=${name}`)
+            );
+            const apiVideogame = response.data.results;
+            const resVideogame = [];
+            apiVideogame.map((game) => {
+                let obj = {
+                    id: game.id,
+                    name: game.name,
+                    background_image: game.background_image,
+                    genres: game.genres,
+                };
+                resVideogame.push(obj);
+            });
+            const quinceResult = resVideogame.slice(0, 15);
+
             res.json(quinceResult);
         } catch (error) {
             res.send("Juego no encontrado");
@@ -57,27 +57,43 @@ router.get("/", async (req, res) => {
 
 router.get("/:idVideogame", async (req, res) => {
     const { idVideogame } = req.params;
-    const response = await axios.get(
-        url.concat(`/${idVideogame}?key=${API_KEY}`)
-    );
-    const apiVideogame = response.data;
-    const resVideogame = {
-        id: apiVideogame.id,
-        name: apiVideogame.name,
-        background_image: apiVideogame.background_image,
-        genres: apiVideogame.genres,
-        description: apiVideogame.description,
-        released: apiVideogame.released,
-        rating: apiVideogame.rating,
-        platforms: apiVideogame.platforms,
-    };
     try {
+        const response = await axios.get(
+            url.concat(`/${idVideogame}?key=${API_KEY}`)
+        );
+        const apiVideogame = response.data;
+        const resVideogame = {
+            id: apiVideogame.id,
+            name: apiVideogame.name,
+            background_image: apiVideogame.background_image,
+            genres: apiVideogame.genres,
+            description: apiVideogame.description,
+            released: apiVideogame.released,
+            rating: apiVideogame.rating,
+            platforms: apiVideogame.platforms,
+        };
+
         res.json(resVideogame);
     } catch (error) {
         res.send("Juego no encontrado");
     }
 });
 
-router.post("/");
+router.post("/", async (req, res) => {
+    const { name, description, releaseDate, rating, platforms } = req.body;
+    try {
+        const newVideogame = await Videogame.create({
+            name,
+            description,
+            releaseDate,
+            rating,
+            platforms,
+        });
+
+        res.send(newVideogame);
+    } catch (error) {
+        res.send(error);
+    }
+});
 
 module.exports = router;
